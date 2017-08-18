1f2f4be366432aa8f7c9bb2247e1d368cbb0c917
A R DANISH <ardnsh001@gmail.com>
2017-08-18 04:15:09 -0700

    Create checkPalindrome.java

---
+++ web_tehnology/basicJava/checkPalindrome.java
@@ -0,0 +1,66 @@
+/*
+check palindrome or not
+*/
+/*
+check name pallindrome
+import java.util.*;
+ 
+class Palindrome
+{
+   public static void main(String args[])
+   {
+      String original, reverse = "";
+      Scanner in = new Scanner(System.in);
+ 
+      System.out.println("Enter a string to check if it is a palindrome");
+      original = in.nextLine();
+ 
+      int length = original.length();
+ 
+      for ( int i = length - 1; i >= 0; i-- )
+         reverse = reverse + original.charAt(i);
+ 
+      if (original.equals(reverse))
+         System.out.println("Entered string is a palindrome.");
+      else
+         System.out.println("Entered string is not a palindrome.");
+ 
+   }
+}	
+*/
+// checking number pallindrome
+public class NoClassDefFoundErrorDueToStaticInitFailure {
+
+    public static void main(String args[]){
+        
+        System.out.println("Please Enter a number : ");
+        int palindrome = new Scanner(System.in).nextInt();
+        
+        if(isPalindrome(palindrome)){
+            System.out.println("Number : " + palindrome + " is a palindrome");
+        }else{
+            System.out.println("Number : " + palindrome + " is not a palindrome");
+        }       
+        
+    }
+  
+  // pallindrome function using in main
+    public static boolean isPalindrome(int number) {
+        int palindrome = number; // copied number into variable
+        int reverse = 0;
+
+        while (palindrome != 0) {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+
+        // if original and reverse of number is equal means
+        // number is palindrome in Java
+        if (number == reverse) {
+            return true;
+        }
+        return false;
+    }
+
+}
